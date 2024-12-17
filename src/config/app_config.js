b47b180bc4b8427a401f54647ef4e9807cacfb01
joregesosa <joregesosa@gmail.com>
2024-12-17 17:09:47 -0400

    added .env file and config folder

---
+++ src/config/app_config.js
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+
+config();
+
+export const appConfig = {
+  port: process.env.APP_PORT,
+  secret: process.env.APP_KEY,
+  url: process.env.APP_URL,
+  env: process.env.APP_ENV,
+  host: process.env.APP_HOST,
+  name: process.env.APP_NAME,
+};
+
+export default appConfig;
