00b18e50ec359a8b292d0654152b31b9b441990b
ChissgaN <coderchissgan@gmail.com>
2024-12-17 17:35:53 -0600

    added .env and config folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "./routes/index.js";
+import appConfig from "./config/app_config.js";
 
 const app = express();
 app.use(express.json());
 
 routes(app);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(appConfig.port, () => {
+  console.log(`Server is running on: ${appConfig.url}`);
 });
